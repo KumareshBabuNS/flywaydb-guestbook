cabe0f2312f3fd622ff2c7d06b8e84c98ab244ed
Jaime Gonzalez Aguilar <jaguilar@pivotal.io>
2015-12-01 17:48:13 -0500

    Update migrate-schema-push-green.sh

--- scripts/migrate-schema-push-green.sh
+++ scripts/migrate-schema-push-green.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-cd ~/AFE\ Demo/corpinfo
-
-echo "Pushing application"
-cf push
-echo "Done pushing application"
 
 echo "Performing Schema Changes"
-cd ~/Downloads/flyway-3.2.1
-./flyway migrate
-echo "Schema changes done- Added Column Ticker"
+echo "------------- Copy FlywayDB new column script to FlywayDB folder tree -------------"
+cp ../flywaydb/V3__new-column.sql $FWDB_HOME/sql/
+echo "------------- Run FlywayDB -------------"
+$FWDB_HOME/flyway migrate
 
-echo "Pushing application V2"
-cd ~/AFE\ Demo/corpinfo-v2
-cf push
-echo "Done pushing application V2"
+echo "------------- Push Green -------------"
+cd spring-boot-guestbook-v2
 
-echo "Performing Another Schema Change"
-cd ~/Downloads/flyway-3.2.1
-cp scripts/V3__not-null-constraint.sql sql/
-cp scripts/V4__add-ticker-value.sql sql/
-./flyway migrate
-echo "Schema changes done- Added Not null Constraint"
+echo "------------- Copy FlywayDB schema creation script to FlywayDB folder tree -------------"
+cp ../flywaydb/V4__not-null-constraint.sql $FWDB_HOME/sql/
+
+echo "------------- Run FlywayDB -------------"
+$FWDB_HOME/flyway migrate
+
+echo "------------- Switch PROD route to Green -------------"
+# TODO
