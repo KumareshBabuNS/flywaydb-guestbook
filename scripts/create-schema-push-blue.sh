15492888a77065312cc6d8d4a93a6b38344d3713
Jaime Gonzalez Aguilar <jaguilar@pivotal.io>
2015-12-01 17:33:35 -0500

    Create create-schema-push-blue.sh

---
+++ scripts/create-schema-push-blue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "------------- Copy FlywayDB schema creation script to FlywayDB folder tree -------------"
+cp ../flywaydb/V2__create-table.sql $FWDB_HOME/sql/
+
+echo "------------- Run FlywayDB -------------"
+$FWDB_HOME/flyway migrate
+
+echo "------------- Push Blue -------------"
+cd spring-boot-guestbook
+cf push
