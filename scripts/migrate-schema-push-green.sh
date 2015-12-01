f0ef95a083f91ceef0e1756dfe79c2679652c921
Jaime Gonzalez Aguilar <jaguilar@pivotal.io>
2015-12-01 17:49:36 -0500

    Update migrate-schema-push-green.sh

--- scripts/migrate-schema-push-green.sh
+++ scripts/migrate-schema-push-green.sh
@@ -9,11 +9,11 @@ $FWDB_HOME/flyway migrate
 echo "------------- Push Green -------------"
 cd spring-boot-guestbook-v2
 
+echo "------------- Switch PROD route to Green -------------"
+# TODO
+
 echo "------------- Copy FlywayDB schema creation script to FlywayDB folder tree -------------"
 cp ../flywaydb/V4__not-null-constraint.sql $FWDB_HOME/sql/
 
 echo "------------- Run FlywayDB -------------"
 $FWDB_HOME/flyway migrate
-
-echo "------------- Switch PROD route to Green -------------"
-# TODO
